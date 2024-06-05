43ab026cf589e534b4e5415fbe6510bbbf753d9d
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2024-06-04 19:56:06 -0500

    fix: updated client components

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -41,7 +41,7 @@ export function HeroSection(): JSX.Element {
 
       <h1 className="animate-fade-up font-urbanist text-5xl font-extrabold tracking-tight sm:text-6xl md:text-7xl lg:text-8xl">
           <Balancer>
-            Save and Pay in USDC Spend local like a{" "}
+            Save and Pay in Digital Dollars Spend local like a{" "}
             <span className="bg-gradient-to-r from-green-400 to-blue-800 bg-clip-text font-extrabold text-transparent">
               Wedi 🔥
             </span>
