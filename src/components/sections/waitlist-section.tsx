58c6c1312d31fa6b2c68f5d506f1a4b91f9e910d
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2024-06-04 20:43:15 -0500

    fix: updated margins on waitlist

--- src/components/sections/waitlist-section.tsx
+++ src/components/sections/waitlist-section.tsx
@@ -5,12 +5,16 @@ export default function WaitlistSection() {
     return (
         <section id="faq-section" aria-label="faq section" className="w-full">
 
+        <div className="container flex max-w-6xl flex-col items-center justify-center gap-8 mt-10 w-full md:mt-22 py-12">
+
+
             <div className="flex flex-row border-border text-muted-foreground text-sm border-2 rounded-full p-2">
                 🚀 We're launching a new payments plaform! Join us!
             </div>
             <div className="w-full max-w-lg md:max-w-xl">
                 <NewsletterSignUpForm />
             </div>
+        </div>
 
         </section>
     )
